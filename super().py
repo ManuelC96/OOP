85c15cf19a7e0f7c9f8f6ebe085eca07f0a07003
ManuelC96 <manuelchiocchetta@gmail.com>
2023-12-08 17:40:48 +0100

    super method implementation

--- super().py
+++ super().py
@@ -1,19 +1,47 @@
 class Human:
-    def __init__(self, sex, age, etnic_group):
+    def __init__(self, name, sex, age, etnic_group):
+        self.name = name
         self.sex = sex
         self.age = age
         self.etnic_group = etnic_group
 
-    def get_pregnant(self):
-        if class.
+    def __repr__(self):
+        if self.__class__.__name__ == "Women":
+            return str(f"{self.name} can give birth to a child")
+            
+        
+        if self.__class__.__name__ == "Men":
+            return str(f"{self.name} can't give birth to a child")
+            
+        
+        if self.__class__.__name__ == "Non_Binary":
+            inp = input("Can you give birth to a child?")
+            if inp == "yes":
+                return str(f"{self.name} can give birth to a child")
+            if inp == "no":
+                return str(f"{self.name} can't give birth to a child")
+        
+         
+
+
+
 class Women(Human):
-    def __init__(self, sex, age, etnic_group)
-        super().__init__(sex, age, etnic_group)
+    def __init__(self, name, sex, age, etnic_group):
+        super().__init__(name, sex, age, etnic_group)
 
 class Men(Human):
-    def __init__(self, sex, age, etnic_group)
-        super().__init__(sex, age, etnic_group)
+    def __init__(self, name, sex, age, etnic_group):
+        super().__init__(name, sex, age, etnic_group)
 
 class Non_Binary(Human):
-    def __init__(self, sex, age, etnic_group)
-        super().__init__(sex, age, etnic_group)
+    def __init__(self, name, sex, age, etnic_group):
+        super().__init__(name, sex, age, etnic_group)
+
+
+
+
+Andrew = Men("Andrew", "male", 11, "caucasian")
+Ariana = Women("Ariana", "female", 25, "caucasian")
+Selena = Non_Binary("Selena", "non binary", 31, "caucasian")
+
+print(Andrew, "\n", Ariana, "\n", Selena)
