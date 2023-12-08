038dd2b41c57f5934c8e7855a248f0fc063f4f6d
ManuelC96 <manuelchiocchetta@gmail.com>
2023-12-08 14:17:42 +0100

    super method implementation

---
+++ super().py
@@ -0,0 +1,19 @@
+class Human:
+    def __init__(self, sex, age, etnic_group):
+        self.sex = sex
+        self.age = age
+        self.etnic_group = etnic_group
+
+    def get_pregnant(self):
+        if class.
+class Women(Human):
+    def __init__(self, sex, age, etnic_group)
+        super().__init__(sex, age, etnic_group)
+
+class Men(Human):
+    def __init__(self, sex, age, etnic_group)
+        super().__init__(sex, age, etnic_group)
+
+class Non_Binary(Human):
+    def __init__(self, sex, age, etnic_group)
+        super().__init__(sex, age, etnic_group)
